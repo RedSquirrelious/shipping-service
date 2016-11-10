12ee5813f5cd7a430357dd91a563f2cd6ab7772e
RedSquirrelious <redsquirrelious@gmail.com>
2016-11-10 15:44:57 -0800

    cleaned up controller

--- app/controllers/shipping_methods_controller.rb
+++ app/controllers/shipping_methods_controller.rb
@@ -1,45 +1,63 @@
 require 'shipping_option'
 
 class ShippingMethodsController < ApplicationController
+
   UPS_LOGIN = ENV["ACTIVESHIPPING_UPS_LOGIN"]
 	UPS_PW = ENV["ACTIVESHIPPING_UPS_PASSWORD"]
 	UPS_KEY = ENV["ACTIVESHIPPING_UPS_KEY"]
 
 		# BASE_URL = 
 	COUNTRY = 'US'
-	ORIGIN_ZIP = 98161
-	DESTINATION_ZIP = 99518
+	ORIGIN_ZIP = 99518
+	DESTINATION_ZIP = 98110
 	
 	WEIGHT = 10
 	UNITS = {units: :imperial}
 	DIMENSIONS = [12, 12, 12]
 # /CONSTANTS
 
+# GOAL:  THE GOAL HERE IS TO KEEP DATA AWAY FROM THE API METHOD
 	def pack_box
 		# @packages = ActiveShipping::Package.new((WEIGHT * 16), DIMENSIONS, UNITS)
-
-		@packages = ActiveShipping::Package.new((WEIGHT.to_i * 16), DIMENSIONS, UNITS)
+		@packages = ActiveShipping::Package.new((WEIGHT * 16), DIMENSIONS, UNITS)
 	end
 
 	def define_origin
+		# @origin = ActiveShipping::Location.new(country: COUNTRY, zip: ORIGIN_ZIP)
 		@origin = ActiveShipping::Location.new(country: COUNTRY, zip: ORIGIN_ZIP)
 	end
 
 	def define_destination
+		# @destination = ActiveShipping::Location.new(country: COUNTRY, zip: DESTINATION_ZIP)
 		@destination = ActiveShipping::Location.new(country: COUNTRY, zip: DESTINATION_ZIP)
+	end
+# /GOAL
+
+  def index	
+		response = ShippingMethodsController.find_UPS_costs(pack_box, define_origin, define_destination)
+  	render json: response
+  end
+
+
+
+	def self.find_UPS_costs(pack_box, define_origin, define_destination)
+		ups = ActiveShipping::UPS.new(login: UPS_LOGIN, password: UPS_PW, key: UPS_KEY)
+
+ 		response = ups.find_rates(define_origin, define_destination, pack_box)
+
+ 		options = []
+
+ 		response.rates.each do |shipping_option|
+ 			estimate = ShippingEstimate.new(shipping_option)
+ 			options << estimate
+ 		end
+
+ 		return options
+
 	end
 
 
-  def index
-  	option = ShippingEstimate.new(pack_box, define_origin, define_destination)
-  	# raise
-  	# puts option.inspect
- 
-  	response = option.find_UPS_costs
-  	# Binding.pry
-  	# render json: JSON.pretty_generate(response)
-  	render json: response
-  end
+
 
   def show
   end
