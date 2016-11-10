1c4463c5a54e84efabe83c0b0bf0e48234760177
RedSquirrelious <redsquirrelious@gmail.com>
2016-11-09 21:48:30 -0800

    not sure if i need this

---
+++ lib/package.rb
@@ -0,0 +1,11 @@
+# class Package < ActiveShipping
+# 	attr_accessor :weight, :dimensions, :units
+
+# 	def initialize(details)
+# 		@weight = details[:weight]
+# 		@dimensions = details[:dimensions]
+# 		@units = details[:units]
+# 	end
+
+# end
+
