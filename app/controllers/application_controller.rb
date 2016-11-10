3ad0cd5e73dd8c8d0ef28bfb6bf0d629c74f46fd
RedSquirrelious <redsquirrelious@gmail.com>
2016-11-09 21:47:13 -0800

    trying to configure api only app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
-class ApplicationController < ActionController::Base
+class ApplicationController < ActionController::API
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  # protect_from_forgery with: :exception
+  #FOLLOWING ABOVE RECOMMENDATION - NOT SURE IF IT'S RIGHT
+  protect_from_forgery with: :null_session
 end
